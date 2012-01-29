57925858fc49a2247b6c78df6a01d962576edff4
os <artemostapetc@gmail.com>
2012-01-30 00:15:50 +0400

    теперь папка views/layouts будет содержать пожпаки admin и site

--- protected/views/layouts/admin/main.php
+++ protected/views/layouts/admin/main.php
@@ -51,7 +51,7 @@
 
 <aside id="sidebar" class="column">
     <form class="quick_search">
-        <input type="text" value="Quick Search" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;">
+        <input type="text" value="Quick Search">
     </form>
     <hr/>
     <h3>Content</h3>
