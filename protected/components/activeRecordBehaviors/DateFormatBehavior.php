adef758115fb4cfacd820a17c4be182b7cff26be
nizsheanez <www.pismeco@gmail.com>
2012-02-02 00:48:20 +0300

    Добавил addTimeDiapasonCondition

--- protected/components/activeRecordBehaviors/DateFormatBehavior.php
+++ protected/components/activeRecordBehaviors/DateFormatBehavior.php
@@ -48,4 +48,33 @@ class DateFormatBehavior extends CActiveRecordBehavior
             }
         }
     }
+
+    /**
+       * Добавляет гораничение по временному диапазону на заданный атрибут.
+       * Виджет для временных диапазонов - FJuiDatePicker
+       *
+       * @param $criteria
+       * @param $attribute_name
+       */
+      public function addTimeDiapasonCondition($criteria, $attribute_name)
+      {
+          $attr = $attribute_name;
+          if (strpos('.',$attribute_name) !== false)
+          {
+              list($prefix, $attr) = explode('.', $attribute_name);
+          }
+          $start = '_' . $attr . '_start';
+          $end   = '_' . $attr . '_end';
+
+          if (isset($_GET[$start]) && ($start = strtotime($_GET[$start])))
+          {
+              $criteria->addCondition($attribute_name . ">'" . date('Y-m-d 00:00:00', $start) . "'");
+          }
+          if (isset($_GET[$end]) && ($end = strtotime($_GET[$end])))
+          {
+              $criteria->addCondition($attribute_name . "<'" . date('Y-m-d 23:59:59', $end) . "'");
+          }
+          return $criteria;
+      }
+
 }
