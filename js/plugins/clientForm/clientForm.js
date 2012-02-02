615934e1007adb6cb1209735eec7f08cd588b6a8
nizsheanez <www.pismeco@gmail.com>
2012-02-03 02:08:14 +0300

    clear form

--- js/plugins/clientForm/clientForm.js
+++ js/plugins/clientForm/clientForm.js
@@ -3,12 +3,11 @@
     $.fn.clientForm = function()
     {
         var self = $(this);
-//        self.tipInput();
     };
 
 })(jQuery);
 
 $(document).ready(function()
 {
-//    $('label').inFieldLabels();
+    $('label').inFieldLabels();
 });
