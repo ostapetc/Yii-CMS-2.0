50e2c38ded37d79baa3a40d5683c279dd4f9c424
nizsheanez <www.pismeco@gmail.com>
2012-03-25 00:55:58 +0400

    почистил экстеншены

---
+++ protected/components/filters/ReturnUrlFilter.php
@@ -0,0 +1,16 @@
+<?php
+class ReturnUrlFilter extends CFilter
+{
+    protected function preFilter($filterChain)
+    {
+        $app     = Yii::app();
+        $request = $app->getRequest();
+
+        if (!$request->getIsAjaxRequest())
+        {
+            $app->getUser()->setReturnUrl($request->getUrl());
+        }
+
+        return true;
+    }
+}
