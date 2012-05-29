05ffb59273237dcf721c36928bff9e1ad7fa2994
os <artem-moscow@yandex.ru>
2012-05-30 00:07:39 +0400

    mnogo vsego kaus za takoy kommit

--- protected/config/constants.php
+++ protected/config/constants.php
@@ -3,7 +3,7 @@
 defined('DS')              || define('DS', DIRECTORY_SEPARATOR);
 defined('YII_DEBUG')       || define('YII_DEBUG', true);
 defined('APP_PATH')        || define('APP_PATH', $_SERVER['DOCUMENT_ROOT'] . 'protected' . DS);
-defined('RUNTIME_PATH')    || define('RUNTIME_PATH', APP_PATH . DS . 'runtime' . DS);
+defined('RUNTIME_PATH')    || define('RUNTIME_PATH', APP_PATH . 'runtime' . DS);
 defined('MODULES_PATH')    || define('MODULES_PATH', APP_PATH . 'modules' . DS);
 defined('LIBRARIES_PATH')  || define('LIBRARIES_PATH', APP_PATH . 'libs' . DS);
 defined('YII_TRACE_LEVEL') || define('YII_TRACE_LEVEL',3);
