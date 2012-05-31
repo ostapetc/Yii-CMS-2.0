b4816acd9c446ea5c2b66a8dc99ccac8947692c1
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-05-31 11:35:19 +0400

    add install sql's

--- protected/config/development.php
+++ protected/config/development.php
@@ -2,10 +2,10 @@
 return CMap::mergeArray(require('main.php'), array(
     'components' => array(
         'db' => array(
-            'connectionString' => 'mysql:host=openserver;dbname=yiicms',
+            'connectionString' => 'mysql:host=localhost;dbname=cms2',
             'emulatePrepare'   => true,
-            'username'         => 'mysql',
-            'password'         => 'mysql',
+            'username'         => 'root',
+            'password'         => '',
             'charset'          => 'utf8',
             'enableProfiling'  => true,
         )
