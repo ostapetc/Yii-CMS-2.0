8eb3168e9e1188a12526f26c1fcef1c30cfc125d
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-06-04 18:26:43 +0400

    done cool modal form style

---
+++ protected/views/layouts/_modalForm.php
@@ -0,0 +1,8 @@
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal">×</button>
+    <h3><?= $modalTitle ?></h3>
+</div>
+<div class="modal-body">
+    <?= $form; ?>
+</div>
+    
