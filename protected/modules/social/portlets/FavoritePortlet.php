8c87a86d91abaa25c0afb133687c3fe225ce9a75
os <artem-moscow@yandex.ru>
2012-06-08 20:53:50 +0400

    favorite module 70%

---
+++ protected/modules/social/portlets/FavoritePortlet.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: os
+ * Date: 07.06.12
+ * Time: 18:47
+ * To change this template use File | Settings | File Templates.
+ */
+class FavoritePortlet
+{
+
+}
---
+++ protected/modules/social/portlets/views/FavoritePortlet.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: os
+ * Date: 07.06.12
+ * Time: 18:48
+ * To change this template use File | Settings | File Templates.
+ */
