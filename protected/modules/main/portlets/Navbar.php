758cceaff30c6b14d6ab742ce13f3ee1e4ca3b70
artem.ostapetc <artemostapetc@gmail.com>
2012-09-03 14:36:55 +0400

    Починка меню

---
+++ protected/modules/main/portlets/Navbar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: artem.ostapetc
+ * Date: 03.09.12
+ * Time: 14:24
+ * To change this template use File | Settings | File Templates.
+ */
+class Navbar
+{
+
+}
