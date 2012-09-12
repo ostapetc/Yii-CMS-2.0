4e512c7fad9b51a92ecb476e5decada5bb995262
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-09-12 15:37:15 +0400

    add install doc

--- protected/modules/docs/DocsModule.php
+++ protected/modules/docs/DocsModule.php
@@ -36,6 +36,7 @@ class DocsModule extends WebModule
         return array(
             '/docs/module/<module:.*>/<view:.*>' => '/docs/docs/module',
             '/docs/<view:.*>' => '/docs/docs/index',
+            '/docs/' => '/docs/docs/index',
             '/docs/base/<view:.*>' => '/docs/docs/index',
             '/docs/base/<folder:\w*>/<view:\w*>' => '/docs/docs/index',
         );
