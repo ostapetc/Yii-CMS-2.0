b9c9ab639f913b3917c0239392179a0df5e3961f
os <artem-moscow@yandex.ru>
2012-09-12 20:19:58 +0300

    задал для тестовых юзеров id. это важно для тестового контента для FK

--- protected/modules/users/migrations/m120908_182118_create_test_user.php
+++ protected/modules/users/migrations/m120908_182118_create_test_user.php
@@ -5,6 +5,7 @@ class m120908_182118_create_test_user extends DbMigration
 	public function up()
 	{
         $user = new User();
+        $user->id       = 1;
         $user->name     = 'Test user';
         $user->email    = 'test@mail.ru';
         $user->password = UserIdentity::crypt('test');
--- protected/modules/users/migrations/m120913_140125_create_test2_user.php
+++ protected/modules/users/migrations/m120913_140125_create_test2_user.php
@@ -5,6 +5,7 @@ class m120913_140125_create_test2_user extends DbMigration
 	public function up()
 	{
         $user = new User();
+        $user->id       = 2;
         $user->name     = 'Test2 user';
         $user->email    = 'test2@mail.ru';
         $user->password = UserIdentity::crypt('test2');
