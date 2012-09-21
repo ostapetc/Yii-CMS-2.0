019239b95e9422a88b49c884f3b38d3afb1888f5
nizsheanez <www.pismeco@gmail.com>
2012-09-21 22:56:27 +0400

    #54 - abstract dp

--- protected/modules/media/components/YouTubeDataProvider.php
+++ protected/modules/media/components/YouTubeDataProvider.php
@@ -1,34 +1,5 @@
 <?php
-class YouTubeDataProvider extends CActiveDataProvider
+class YouTubeDataProvider extends ApiDataProviderAbstract
 {
-    private $_criteria;
-
-    /**
-     * @var Zend_Gdata_YouTube
-     */
-
-    public function __construct($model,$config=array())
-    {
-        $modelClass = get_class($model);
-        $this->model = $model;
-        $this->modelClass = $modelClass;
-        parent::__construct($model, $config);
-    }
-
-    public function getCriteria()
-    {
-        if($this->_criteria===null)
-            $this->_criteria=new YouTubeApiCriteria;
-        return $this->_criteria;
-    }
-
-    public function setCriteria($value)
-    {
-        $this->_criteria=$value instanceof YouTubeApiCriteria ? $value : new CDbCriteria($value);
-    }
-
-    public function getSort()
-    {
-        return false;
-    }
+    protected $criteriaClass = 'YouTubeApiCriteria';
 }
