914970b88b5f6e3bcde56b9e3d63e8d8ef9a6494
nizsheanez <www.pismeco@gmail.com>
2012-10-01 18:54:14 +0400

    small security fix

--- protected/components/activeRecordBehaviors/FileManagerBehavior.php
+++ protected/components/activeRecordBehaviors/FileManagerBehavior.php
@@ -20,16 +20,24 @@ class FileManagerBehavior extends ActiveRecordBehavior
                 CActiveRecord::HAS_MANY,
                 $storage_class,
                 "object_id",
-                'condition' => "$tag.model_id = '" . get_class($model) . "' AND $tag.tag='$tag'",
-                'order'     => "$tag.order DESC"
+                'condition' => "$tag.model_id=:model_id AND $tag.tag=:tag",
+                'order'     => "$tag.order DESC",
+                'params'    => array(
+                    'model_id' => get_class($model),
+                    'tag' => $tag
+                )
             ));
             $rel = $tag.'_first';
             $model->getMetaData()->addRelation($rel, array(
                 CActiveRecord::HAS_ONE,
                 $storage_class,
                 'object_id',
-                'condition' => "$rel.model_id = '" . get_class($model) . "' AND $rel.tag='$tag'",
-                'order'     => "$rel.order DESC"
+                'condition' => "$rel.model_id=:model_id AND $rel.tag=:tag",
+                'order'     => "$rel.order DESC",
+                'params'    => array(
+                    'model_id' => get_class($model),
+                    'tag' => $tag
+                )
             ));
         }
     }
