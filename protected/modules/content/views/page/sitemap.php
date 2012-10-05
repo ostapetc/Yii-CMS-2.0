45c3730d0c095c1e18275d2866b4d11e727ee4d9
nizsheanez <www.pismeco@gmail.com>
2012-10-05 22:14:57 +0400

    #64 - site map

---
+++ protected/modules/content/views/page/sitemap.php
@@ -0,0 +1,51 @@
+<?
+$this->widget('ext.slickmapCss.SlickMap', array(
+    'items' => array(
+        array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+        array('label'=>'Home1', 'url'=>array('site/index'), 'active' => false,
+            'items' => array(
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false,
+                      'items' => array(
+                          array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                          array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                          array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                      )
+                ),
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+            )
+        ),
+        array('label'=>'Home2', 'url'=>array('site/index'), 'active' => false,
+            'items' => array(
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+            )
+        ),
+        array('label'=>'Home3', 'url'=>array('site/index'), 'active' => false),
+        array('label'=>'Home4', 'url'=>array('site/index'), 'active' => false,
+              'items' => array(
+                  array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                  array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                  array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                  array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                  array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                  array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+              )
+        ),
+        array('label'=>'Home5', 'url'=>array('site/index'), 'active' => false,
+            'items' => array(
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false,
+                    'items' => array(
+                        array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                        array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                        array('label'=>'Home0', 'url'=>array('site/index'), 'active' => false),
+                    )
+                ),
+            )
+        ),
+        array('label'=>'Home5', 'url'=>array('site/index'), 'active' => false),
+    )
+));
