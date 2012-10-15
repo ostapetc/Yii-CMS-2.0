f9a36574dcd2842053032430dea8edf312f8fba7
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-10-15 14:09:56 +0400

    clear

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 
-. props.sh
-
-
 action=$(whiptail --clear --menu 'What do you want to do?' 13 30 5 'deploy' '' 'rollback' '' 'remove-maintenance-page' '' 'remove-deploy-lock' '' 3>&1 1>&2$
 . $action;
 
