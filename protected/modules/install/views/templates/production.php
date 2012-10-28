8cd00249761231a8b9d6ebde1c7ea45d30c7c028
nizsheanez <www.pismeco@gmail.com>
2012-10-28 22:44:19 +0400

    add more commands

--- protected/modules/install/views/templates/production.php
+++ protected/modules/install/views/templates/production.php
@@ -8,8 +8,13 @@ return CMap::mergeArray(require ('main.php'), array(
              'password'              => '{{DB_PASS}}',
              'charset'               => 'utf8',
              'enableProfiling'       => true,
-             'initSQLs'=>array("SET NAMES 'utf8' COLLATE 'utf8_general_ci'")
-
+             'initSQLs'=>array("
+                 SET NAMES 'utf8' COLLATE 'utf8_general_ci';
+                 SET CHARACTER SET 'utf8';
+                 SET character_set_client = 'utf8';
+                 SET character_set_results = 'utf8';
+                 SET character_set_connection = 'utf8';
+             ")
          ),
      )
 ));
