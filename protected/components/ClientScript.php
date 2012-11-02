be97a99fee53f3d836943d75c7b7f53de86624c8
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-11-02 12:53:47 +0400

    #88 - add comments

--- protected/components/ClientScript.php
+++ protected/components/ClientScript.php
@@ -1,6 +1,15 @@
 <?php
 class ClientScript extends CClientScript
 {
+
+    /**
+     * add JSONP-supporting functionality
+     * using: $.getJSON('url?callback=?', function(html) {alert(html);});
+     * see(http://api.jquery.com/jQuery.getJSON/#jsonp)
+     * all registered scripts (non file) will evaluate on client automatically
+     *
+     * @param string $output
+     */
     public function render(&$output)
     {
         if (Yii::app()->request->getIsAjaxRequest() && isset($_GET['callback']))
