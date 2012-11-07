4eb67a06f153d3bdad2c923278b5d46b0ec5c18a
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-11-07 17:16:56 +0400

    deploy up

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-action=$(whiptail --clear --menu 'What do you want to do?' 13 30 5 'deploy' '' 'rollback' '' 'remove-maintenance-page' '' 'remove-deploy-lock' '' 3>&1 1>&2$
+action=$(whiptail --clear --menu 'What do you want to do?' 13 30 5 'deployment' '' 'rollback' '' 'remove-maintenance-page' '' 'remove-deploy-lock' '' 3>&1 1>&2)
 . $action;
 
 
